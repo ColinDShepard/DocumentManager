81bcbab25e6a3bffe45c2c8644ec4d1a4231935f
Het Shah <shahhet11@gmail.com>
2022-12-13 17:38:02 -0800

    While clicking delete button, it was not displaying "Not Found" icon when we delete the last file in any User's account

--- DocumentManager/DocumentManager/HomePage.xaml.cs
+++ DocumentManager/DocumentManager/HomePage.xaml.cs
@@ -181,7 +181,7 @@ public partial class HomePage : ContentPage
         pickerDoStuff();
         if (isDeleteClicked && arrayList.Count == 0)
         {
-            Image image1 = new Image { Source = ImageSource.FromUri(new Uri("http://www.testurl.com")) };
+            Image image1 = new Image { Source = ImageSource.FromUri(new Uri("https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png")) };
             TestImage.Source = image1.Source;
 
         }
